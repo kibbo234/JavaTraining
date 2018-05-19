74ba33ee4bf3b701aebc96edf9e67838ff22186b
kibbo <kibbo234@gmail.com>
2018-05-19 19:25:45 +0800

    离线操作

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.System;
+import static java.lang.System.out;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Main{
+    public static void main(String[] args){
+        WindowMenu win = new WindowMenu("带菜单的窗口",20,30,200,190);
+
+    }
+}
